efc1ea6177a7d83e8369b09e2542fd50da2a76b3
unknown <davidgirges1@gmail.com>
2026-03-01 00:09:23 +0200

    28-2

---
+++ script.js
@@ -0,0 +1,99 @@
+// window.alert("hello world of coding");
+// document.write("<h2>hello this is h2</h2>");
+// console.log("hello js console")
+// console.error("this is a created error");
+// console.table(["david", "ahmed", "khalid"]);
+
+// console.log(
+//   "hello from %cjs %cfile",
+//   "color:red; font-size: 40px",
+//   "color:blue; font-size: 40px",
+// );
+
+// console.log(typeof 12); // number
+// console.log(typeof 12.0); // number
+// console.log(typeof "David"); // string
+// console.log(typeof [1, 2, 3]); // object
+// console.log(typeof { name: "david", age: 20, country: "Egypt" }); // object
+// console.log(typeof false); // boolean
+// console.log(typeof true); // boolean
+// console.log(typeof null); // object
+// console.log(typeof undefined); // undefined
+
+// var user = "David";
+// console.log(user);
+
+// var name = "dAvid",
+//   nickname = "dodo";
+// console.log(nickname);
+
+// ID is considered a global variable
+
+// JS is a loosly typed language. type of var determined during execution
+// static typed. you have to determine the type before execution
+
+// identifiers: you can use $ _ at the beginning
+// camelCase naming convention is used
+// let myName = "David";
+
+// let and const doesn allwo redeclare unlike var (old)
+// access before declare is more error detailed in let and const
+// console.log(x);
+// let x = 12; // detailed error emssage unlike var
+
+// variable scope drama: any var created with the var is put in the window object
+// var msg = "hello";
+// console.log(window.msg); // can be accessed here
+
+// Escape characters
+// console.log('I \'m David');
+// console.log("I \\ love backslash");
+// console.log("David \n elks");
+
+// concatination (+)
+// console.log("my name is " + "David");
+// console.log("David", "Elks"); // auto puts space in between
+
+//template strings  (` `)
+// let name = "david";
+// let age = 19;
+// let country = "Egypt";
+// console.log(`my name is ${name} and my age is
+// ${age} and I live in ${country} """ // '' no escaping required`);
+
+// you can also write in new lines without the /n
+// let markup = `
+//     <div>
+//         <h1> hello world </h1>
+//     </div>
+// `;
+// document.write(markup);
+
+// Variables and concatination challenge
+// let myTitle = "Elxero",
+//   myDesc = "the best course ever",
+//   myDate = "28-2-2026";
+
+// let markUp = `
+//     <div>
+//         <h3>${myTitle} </h1>
+//         <p>${myDesc} </p>
+//         <span>${myDate} </span>
+//     </div>
+// `;
+
+// document.write(markUp);
+// document.write(markUp);
+// document.write(markUp);
+
+// Arithmatic operators
+// console.log(10 + " David");
+// console.log(10 - " David"); // NaN not a number
+// console.log(typeof NaN); // number
+// console.log(2 ** 4);
+// console.log(10 % 2); // check even or odd
+// var num = 10;
+// console.log(num++); // 10
+// console.log(num); // 11
+
+// unary operators -- to be continued
